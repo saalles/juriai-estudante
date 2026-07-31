5146edeae0cbdcc9b7aa3ef0eb29a29896ff31a3
saalles <168289849+saalles@users.noreply.github.com>
2026-07-31 19:29:55 -0400

    Increase JSON body limit to 15mb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "15mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
